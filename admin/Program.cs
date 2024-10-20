88c6baa16ac715f9d2ad6a6e2c9c0b03971b2a99
Tim Hanewich <tahanewich@live.com>
2024-10-20 09:41:06 -0400

    Added admin C# project

---
+++ admin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Spectre.Console;
+
+namespace CMonitorAdministration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
