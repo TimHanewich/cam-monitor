255aa4e09a9f394d327d5b128f4f34608cb219a6
Tim Hanewich <tahanewich@live.com>
2024-10-20 13:26:18 -0400

    Function to check most recent

--- admin/Program.cs
+++ admin/Program.cs
@@ -16,102 +16,160 @@ namespace CMonitorAdministration
 
         public async static Task MainProgramAsync()
         {
-            SelectionPrompt<string> ToDo = new SelectionPrompt<string>();
-            ToDo.Title("What do you want to do?");
-            ToDo.AddChoice("Download images between a date range");
-
-            //Ask
-            string WantToDo = AnsiConsole.Prompt(ToDo);
-            if (WantToDo == "Download images between a date range")
+            while (true)
             {
-                //Authenticate w/ azure blob storage
-                AnsiConsole.Markup("[italic]Setting up blob storage...[/] ");
-                BlobServiceClient bsc = new BlobServiceClient(GetAzureBlobStorageConnectionString());
-                BlobContainerClient bcc = bsc.GetBlobContainerClient("cmonitor-images");
-                if (await bcc.ExistsAsync() == false)
-                {
-                    await bcc.CreateAsync();
-                }
-                AnsiConsole.MarkupLine("[green]set up![/]");
+                SelectionPrompt<string> ToDo = new SelectionPrompt<string>();
+                ToDo.Title("What do you want to do?");
+                ToDo.AddChoice("Download images between a date range");
+                ToDo.AddChoice("Check most recent image upload");
 
-                //Get begin date?
-                DateTime starting = DateTime.UtcNow.AddDays(-7); //default to 1 week
-                Console.Write("Starting when? (in MM/DD/YYYY format) > ");
-                string? sw = Console.ReadLine();
-                if (sw != null)
+                //Ask
+                string WantToDo = AnsiConsole.Prompt(ToDo);
+                if (WantToDo == "Download images between a date range")
                 {
-                    starting = DateTime.Parse(sw);
-                }
-
-                //Get end date
-                DateTime ending = DateTime.UtcNow; //default to now
-                Console.Write("Ending when? (in MM/DD/YYYY format) > ");
-                string? ew = Console.ReadLine();
-                if (ew != null)
-                {
-                    ending = DateTime.Parse(ew);
-                }
-
-                //Get in between dates
-                Console.WriteLine();
-                DateTime[] AllDatesToGet = GetDatesBetween(starting, ending);
-                AnsiConsole.MarkupLine("Going to download [bold]" + AllDatesToGet.Length.ToString("#,##0") + "[/] days worth of photos:");
-                foreach (DateTime dt in AllDatesToGet)
-                {
-                    Console.WriteLine("- " + dt.ToShortDateString());
-                }
-
-                //Get list of all photos
-                List<string> PhotosToDownload = new List<string>();
-                Console.WriteLine();
-                foreach (DateTime dt in AllDatesToGet)
-                {
-                    AnsiConsole.Markup("Querying photos for [bold][navy]" + dt.ToShortDateString() + "[/][/]... ");
-                    Azure.Pageable<BlobItem> items = bcc.GetBlobs(prefix: dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00"));
-                    AnsiConsole.MarkupLine("[bold]" + items.Count().ToString("#,##0") + "[/] photos found");
-                    foreach (BlobItem bi in items)
+                    //Authenticate w/ azure blob storage
+                    AnsiConsole.Markup("[italic]Setting up blob storage...[/] ");
+                    BlobServiceClient bsc = new BlobServiceClient(GetAzureBlobStorageConnectionString());
+                    BlobContainerClient bcc = bsc.GetBlobContainerClient("cmonitor-images");
+                    if (await bcc.ExistsAsync() == false)
                     {
-                        PhotosToDownload.Add(bi.Name);
+                        await bcc.CreateAsync();
+                    }
+                    AnsiConsole.MarkupLine("[green]set up![/]");
+
+                    //Get begin date?
+                    DateTime starting = DateTime.UtcNow.AddDays(-7); //default to 1 week
+                    Console.Write("Starting when? (in MM/DD/YYYY format) > ");
+                    string? sw = Console.ReadLine();
+                    if (sw != null)
+                    {
+                        starting = DateTime.Parse(sw);
+                    }
+
+                    //Get end date
+                    DateTime ending = DateTime.UtcNow; //default to now
+                    Console.Write("Ending when? (in MM/DD/YYYY format) > ");
+                    string? ew = Console.ReadLine();
+                    if (ew != null)
+                    {
+                        ending = DateTime.Parse(ew);
+                    }
+
+                    //Get in between dates
+                    Console.WriteLine();
+                    DateTime[] AllDatesToGet = GetDatesBetween(starting, ending);
+                    AnsiConsole.MarkupLine("Going to download [bold]" + AllDatesToGet.Length.ToString("#,##0") + "[/] days worth of photos:");
+                    foreach (DateTime dt in AllDatesToGet)
+                    {
+                        Console.WriteLine("- " + dt.ToShortDateString());
+                    }
+
+                    //Get list of all photos
+                    List<string> PhotosToDownload = new List<string>();
+                    Console.WriteLine();
+                    foreach (DateTime dt in AllDatesToGet)
+                    {
+                        AnsiConsole.Markup("Querying photos for [bold][navy]" + dt.ToShortDateString() + "[/][/]... ");
+                        Azure.Pageable<BlobItem> items = bcc.GetBlobs(prefix: dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00"));
+                        AnsiConsole.MarkupLine("[bold]" + items.Count().ToString("#,##0") + "[/] photos found");
+                        foreach (BlobItem bi in items)
+                        {
+                            PhotosToDownload.Add(bi.Name);
+                        }
+                    }
+
+                    //Ready!
+                    Console.WriteLine();
+                    AnsiConsole.Markup("[italic][gray]Ready to download [navy]" + PhotosToDownload.Count.ToString("#,##0") + " photos[/] when you are. Enter to proceed.[/][/]");
+                    Console.ReadLine();
+
+                    //Make a folder for us to download in
+                    string DownloadPath = Path.Combine("./", "download-" + Guid.NewGuid().ToString().Replace("-", ""));
+                    System.IO.Directory.CreateDirectory(DownloadPath);
+
+                    //Download all
+                    Console.WriteLine();
+                    AnsiConsole.MarkupLine("Proceeding to download [bold][navy]" + PhotosToDownload.Count.ToString("#,##0") + "[/][/] over [bold][navy]" + AllDatesToGet.Length.ToString("#,##0") + "[/][/] dates!");
+                    for (int t = 0; t < PhotosToDownload.Count; t++)
+                    {    
+
+                        //Print what we're doing
+                        string blobname = PhotosToDownload[t];
+                        float percent = Convert.ToSingle(t) / Convert.ToSingle(PhotosToDownload.Count);
+                        AnsiConsole.Markup("[gray](" + t.ToString("#,##0") + " / " + PhotosToDownload.Count.ToString("#,##0") + ", " + percent.ToString("#0.0%") + ")[/]" + " Downloading [bold][navy]" + blobname + "[/][/]... ");
+                        
+                        //Download!
+                        string destpath = Path.Combine(DownloadPath, blobname);
+                        FileStream fs = System.IO.File.Create(destpath); //Create the file
+                        fs.Close();
+                        BlobClient bc = bcc.GetBlobClient(blobname);
+                        if (await bc.ExistsAsync() == false)
+                        {
+                            continue;
+                        }
+                        await bc.DownloadToAsync(destpath);
+                        AnsiConsole.MarkupLine("[green]Downloaded![/]");
+                    }
+
+                    //Print
+                    Console.WriteLine();
+                    AnsiConsole.MarkupLine("[green]" + PhotosToDownload.Count.ToString("#,##0") + " photos downloaded to '" + System.IO.Path.GetFullPath(DownloadPath) + "'![/]");
+                    Console.WriteLine();
+                }
+                else if (WantToDo == "Check most recent image upload")
+                {
+                    //Authenticate w/ azure blob storage
+                    AnsiConsole.Markup("[italic]Setting up blob storage...[/] ");
+                    BlobServiceClient bsc = new BlobServiceClient(GetAzureBlobStorageConnectionString());
+                    BlobContainerClient bcc = bsc.GetBlobContainerClient("cmonitor-images");
+                    if (await bcc.ExistsAsync() == false)
+                    {
+                        await bcc.CreateAsync();
+                    }
+                    AnsiConsole.MarkupLine("[green]set up![/]");
+
+                    //Begin search
+                    DateTime FurthestSearch = DateTime.UtcNow.AddDays(-7);
+                    DateTime[] ToSearch = GetDatesBetween(FurthestSearch, DateTime.UtcNow);
+                    bool answered = false;
+                    for (int t = 0; t < ToSearch.Length; t++)
+                    {
+                        if (answered == false)
+                        {
+                            DateTime ToSearchNow = ToSearch[ToSearch.Length - 1 - t]; //Inverse direction
+                            AnsiConsole.Markup("Searching on date [bold]" + ToSearchNow.ToShortDateString() + "[/]... ");
+                            Azure.Pageable<BlobItem> items = bcc.GetBlobs(prefix: ToSearchNow.Year.ToString("0000") + ToSearchNow.Month.ToString("00") + ToSearchNow.Day.ToString("00")); //Search on the date (using prefix)
+                            AnsiConsole.MarkupLine("[bold]" + items.Count().ToString("#,##0") + "[/] photos found");
+                            
+                            //If there are items, get the most recent one!
+                            if (items.Count() > 0)
+                            {
+                                //Get the most recent
+                                DateTime MostRecent = new DateTime(1900, 1, 1); //Default date (a long time ago so every date is more recent than this!)
+                                foreach (BlobItem item in items)
+                                {
+                                    string timestamp = item.Name.Replace(".jpg", "");
+                                    DateTime thisstamp = TimeStamper.TimeStampToDateTime(timestamp);
+                                    if (thisstamp > MostRecent)
+                                    {
+                                        MostRecent = thisstamp;
+                                    }
+                                }
+
+                                //Return the most recent
+                                TimeSpan ts = DateTime.UtcNow - MostRecent;
+                                AnsiConsole.MarkupLine("The last picture that was taken was taken at " + MostRecent.ToString() + " UTC");
+                                AnsiConsole.MarkupLine("That was " + ts.TotalMinutes.ToString("#,##0") + " minutes ago!");
+                                Console.WriteLine();
+                                answered = true; //Mark as answered so other loops know to ignore!
+                            }
+                        }
                     }
                 }
-
-                //Ready!
-                Console.WriteLine();
-                AnsiConsole.Markup("[italic][gray]Ready to download [navy]" + PhotosToDownload.Count.ToString("#,##0") + " photos[/] when you are. Enter to proceed.[/][/]");
-                Console.ReadLine();
-
-                //Make a folder for us to download in
-                string DownloadPath = Path.Combine("./", "download-" + Guid.NewGuid().ToString().Replace("-", ""));
-                System.IO.Directory.CreateDirectory(DownloadPath);
-
-                //Download all
-                Console.WriteLine();
-                AnsiConsole.MarkupLine("Proceeding to download [bold][navy]" + PhotosToDownload.Count.ToString("#,##0") + "[/][/] over [bold][navy]" + AllDatesToGet.Length.ToString("#,##0") + "[/][/] dates!");
-                for (int t = 0; t < PhotosToDownload.Count; t++)
-                {    
-
-                    //Print what we're doing
-                    string blobname = PhotosToDownload[t];
-                    float percent = Convert.ToSingle(t) / Convert.ToSingle(PhotosToDownload.Count);
-                    AnsiConsole.Markup("[gray](" + t.ToString("#,##0") + " / " + PhotosToDownload.Count.ToString("#,##0") + ", " + percent.ToString("#0.0%") + ")[/]" + " Downloading [bold][navy]" + blobname + "[/][/]... ");
-                    
-                    //Download!
-                    string destpath = Path.Combine(DownloadPath, blobname);
-                    FileStream fs = System.IO.File.Create(destpath); //Create the file
-                    fs.Close();
-                    BlobClient bc = bcc.GetBlobClient(blobname);
-                    if (await bc.ExistsAsync() == false)
-                    {
-                        continue;
-                    }
-                    await bc.DownloadToAsync(destpath);
-                    AnsiConsole.MarkupLine("[green]Downloaded![/]");
+                else
+                {
+                    Console.WriteLine("I don't know what that is!");
                 }
-
-                //Print
-                Console.WriteLine();
-                AnsiConsole.MarkupLine("[green]" + PhotosToDownload.Count.ToString("#,##0") + " photos downloaded to '" + System.IO.Path.GetFullPath(DownloadPath) + "'![/]");
-                Console.WriteLine();
             }
         }
 
--- admin/TimeStamper.cs
+++ admin/TimeStamper.cs
@@ -9,6 +9,50 @@ namespace CMonitorAdministration
             return dt.Year.ToString("0000") + dt.Month.ToString("00") + dt.Day.ToString("00") + dt.Hour.ToString("00") + dt.Second.ToString("00");
         }
 
+        public static DateTime TimeStampToDateTime(string ts)
+        {
+            //Parse out
+            int year = 0;
+            int month = 0;
+            int day = 0;
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+
+            //Parse now
+            if (ts.Length >= 4)
+            {
+                year = Convert.ToInt32(ts.Substring(0, 4));
+            }
+            if (ts.Length >= 6)
+            {
+                month = Convert.ToInt32(ts.Substring(4, 2));
+            }
+            if (ts.Length >= 8)
+            {
+                day = Convert.ToInt32(ts.Substring(6, 2));
+            }
+            if (ts.Length >= 10)
+            {
+                hour = Convert.ToInt32(ts.Substring(8, 2));
+            }
+            if (ts.Length >= 12)
+            {
+                minute = Convert.ToInt32(ts.Substring(10, 2));
+            }
+            if (ts.Length >= 14)
+            {
+                second = Convert.ToInt32(ts.Substring(12, 2));
+            }
+            //Console.WriteLine("Year: " + year.ToString());
+            //Console.WriteLine("Month: " + month.ToString());
+            //Console.WriteLine("Day: " + day.ToString());
+            //Console.WriteLine("Hour: " + hour.ToString());
+            //Console.WriteLine("Minute: " + minute.ToString());
+            //Console.WriteLine("Second: " + second.ToString());
+            return new DateTime(year, month, day, hour, minute, second);
+        }
+
         public enum Depth
         {
             Seconds = 0,
